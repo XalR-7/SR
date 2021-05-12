53aca66a8ac8720a8bb20b0b895b3937a7e78181
XalR-7 <81435656+XalR-7@users.noreply.github.com>
2021-05-12 10:48:59 -0400

    Sr.py

---
+++ Sr.py
@@ -0,0 +1,41 @@
+# sr.py import time
+import os
+print(“””hello, now loading SR tool for termux or kali linux.
+
+Copy right @2021 SR Team
+
+NO SKIDS ALLOWED“””)
+time.sleep(04)
+os.system(‘clear’)
+def banner:
+      print(“”” THE
+  HOES 
+       ATTACK
+ LOVE.            SR CLAN”””)
+
+def menu():
+  print("1: Makers")
+  print("2: exit")
+def fun():
+  choice=("1")
+  banner()
+  
+  while choice != ("2"):
+    menu()
+    choice=input("Enter your choice:")
+    if choice == ("1")
+    time.sleep(1)
+    print("made by ricin for xal")
+    
+        elif choice == ("2"):
+            time.sleep(1)
+            print("bai")
+
+        else:
+            print("incorrect")
+
+
+
+
+if __name__ == "__main__":
+    fun()
